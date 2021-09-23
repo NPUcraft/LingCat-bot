0bebb475e17b797c11058ced7640a02fdb931bac
Sirius0v0 <1694605269@qq.com>
2021-09-23 23:01:44 +0800

    fix bugs

--- plugins/plugin-ban.js
+++ plugins/plugin-ban.js
@@ -8,8 +8,10 @@ const { getPermission } = require("../lib/permission");
 
 async function banned(_bot, data) {
     // if (!await getPermission(data, "closeAll")) return;
+    if (data.user_id !== _bot.uin) return;
     let permission = _readFileSync(permissionDir, "permission");
     const gid = String(data.group_id);
+    if (typeof permission?.[gid] === "undefined") return;
 
     if (data.duration > 0) {    // 被禁言
         permission[gid]["banned"] = true;
