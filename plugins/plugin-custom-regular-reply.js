a40a3eeb014c40c7f85e2218569df1d3bfd74f25
SanDiegoPolis <1368616836@qq.com>
2021-12-13 23:15:35 +0800

    设置默认正则回复

--- plugins/plugin-custom-regular-reply.js
+++ plugins/plugin-custom-regular-reply.js
@@ -168,9 +168,9 @@ async function customRegReply(_bot, data, args) {
         let join_time = new Date(res.data.join_time * 1e3);
         let time = Date.now() - join_time;
         let newTime = 3;// 判定为新人的时间，单位：天
-        let isNew = ((time / 86400000 < newTime) || data.sender.level == 1)?true:false;
-        // 判断是不是号主
-        let isOwner = data.user_id == accountInfo["owner"]?true:false;
+        let isNew = ((time / 86400000 < newTime) && data.sender.level == 1)?true:false;
+        // 判断是不是号主&管理
+        let isOwner = (data.user_id == accountInfo["owner"] || ["admin", "owner"].indexOf(data.sender.role) !== -1)?true:false;
         //let isOwner = false;
         // 判断是不是机器人id
         let isRobot = (data.user_id == 2987084315 || // 灵喵
@@ -185,12 +185,15 @@ async function customRegReply(_bot, data, args) {
             const gid = data.group_id.toString();
             let replyData = _readFileSync(replyDir, "customRegReply");
             let defalutReplyData = _readFileSync(replyDir, "customRegReply-default");
-
             let patternObj = replyData[gid]["pattern"];
-            patternObj = Object.assign(patternObj, defalutReplyData["default"]["pattern"]); // 此行拼合了自定义对象和默认对象
-            console.log(patternObj);
-            for (let key in patternObj) 
-            {
+            let replyObj = replyData[gid]["reply"];
+
+            if (["710085830","598445021","543559290"].indexOf(gid) != -1) { // 暂时只在测试群，大群，社团群开启默认回复
+                patternObj = Object.assign(patternObj, defalutReplyData["default"]["pattern"]); // 此行拼合了自定义对象和默认对象，对于相同键值对，默认会覆盖自定义的
+                replyObj = Object.assign(replyObj, defalutReplyData["default"]["reply"]);       // 此行拼合了自定义对象和默认对象，对于相同键值对，默认会覆盖自定义的
+            }
+            
+            for (let key in patternObj) {
                 let value = patternObj[key];
                 let rawPatt = new String(value).slice(String(value).indexOf("/"), String(value).lastIndexOf("/") + 1); // 用第一个和最后一个"/"分割
                 let rawMod = new String(value).slice(String(value).lastIndexOf("/") + 1).split("").sort().join(""); // 构造函数会把修饰符gims重新排序，所以需要sort之后比较
@@ -199,29 +202,13 @@ async function customRegReply(_bot, data, args) {
                 try {
                     re = new RegExp(rawPatt.slice(1, -1), rawMod.toString());
                     if (String(args).match(re) != null) {
-                        data.reply(`[CQ:at,qq=${data.user_id}]\n` + replyData[gid]["reply"][key]); // 正则回复核心代码
+                        data.reply(`[CQ:at,qq=${data.user_id}]\n` + replyObj[key]); // 正则回复核心代码
                     }
                     //console.log(re);
                 } catch(err) {
                     console.log(err);
                 }
             }
-
-            // if (RegExp(/考核服/).test(data.message[0].data.text) == true) {
-            //     data.reply("[CQ:at,qq=" + data.sender.user_id + "]" + "\n请阅读常见问题解答：https://wiki.npucraft.com/npucraftwiki/index.php/%E5%B8%B8%E8%A7%81%E9%97%AE%E9%A2%98%E8%A7%A3%E7%AD%94");  
-            // }
-            // if (RegExp(/基岩版/).test(data.message[0].data.text) == true || RegExp(/手机/).test(data.message[0].data.text) == true && RegExp(/最新版手机QQ/).test(data.message[0].data.text) == false) {
-            //     data.reply("[CQ:at,qq=" + data.sender.user_id + "]" + "\n本服务器可以使用基岩版进服，群文件→基岩版（互通专供）安装包 可获取安装包。\n详请阅读：https://wiki.npucraft.com/npucraftwiki/index.php/%E5%9F%BA%E5%B2%A9%E7%89%88%E4%BA%92%E9%80%9A");
-            // }
-            // if (RegExp(/java/i).test(data.message[0].data.text) == true) {
-            //     data.reply("[CQ:at,qq=" + data.sender.user_id + "]" + "\nMC1.17之后必须安装Java1.16或更高版本，下载链接: https://mirrors.tuna.tsinghua.edu.cn/AdoptOpenJDK/16/jre/x64/windows/OpenJDK16U-jre_x64_windows_hotspot_16.0.1_9.msi");
-            // }
-            // if (RegExp(/(服).*(版本)/).test(data.message[0].data.text) == true) {
-            //     data.reply("[CQ:at,qq=" + data.sender.user_id + "]" + "\n生存服版本1.17.1");
-            // }
-            // if (RegExp(/建筑服/).test(data.message[0].data.text) == true || RegExp(/复原/).test(data.message[0].data.text) == true) {
-            //     data.reply("[CQ:at,qq=" + data.sender.user_id + "]" + "\n本服务器建筑服目前正在社团内部进行删档测试。本校学生若想参与复原工程可联系群管理SUPER2FH。\n进入建筑服硬性要求：\n1.非观光摸鱼党；\n2.需要为西工大在校生或毕业生。");
-            // }
         }
     })
 }
