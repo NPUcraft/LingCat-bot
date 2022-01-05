311393b958ed1123e869cecfe2f4c6de4243ceec
Sirius0v0 <1694605269@qq.com>
2022-01-05 16:28:20 +0800

    今日人品节日彩蛋

--- plugins/plugin-jrrp.js
+++ plugins/plugin-jrrp.js
@@ -1,6 +1,7 @@
 "use strict"
 const seedRandom = require("../lib/seed-random");
 const { getPermission } = require("../lib/permission");
+const goodNumber = [666, 888, 188, 648, 288, 600, 800, 999, 1314, 168, 199, 99, 100, 520, 521, 258, 88];
 const help = `
 查看今日人品
 `.trim();
@@ -13,10 +14,11 @@ async function jrrp(_bot, data, args = null) {
     } else if (args?.length > 1) {
         return;
     }
-    
-    const seedID = data.sender.user_id + new Date().toLocaleDateString();
-    const randomNum = seedRandom.getRandomIntInclusive(seedID, -20, 120);
+    let today = new Date();
+    const seedID = data.sender.user_id + today.toLocaleDateString();
+    let randomNum = seedRandom.getRandomIntInclusive(seedID, -20, 120);
     let sender = data.sender.card ? data.sender.card : data.sender.nickname;
+    randomNum = (today.getTime() > new Date(2022, 0, 30).getTime() && today.getTime() < new Date(2022, 1, 14).getTime()) ? goodNumber[(randomNum % goodNumber.length + goodNumber.length) % goodNumber.length] : randomNum;
     data.reply(`${sender} 今日的人品为 ${randomNum}`);
 }
 exports.jrrp = jrrp;
