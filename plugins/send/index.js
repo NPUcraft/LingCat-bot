8f242678fc22fda429ceb699e2adad34711b462b
Sirius0v0 <1694605269@qq.com>
2022-06-08 12:07:41 +0800

    :bug:修复图片无法显示的bug

--- plugins/send/index.js
+++ plugins/send/index.js
@@ -1,6 +1,7 @@
 console.log('plugin-send loaded');
 
 import _ from "lodash";
+import { segment } from "oicq";
 import { loadFileAsJson } from "../../lib/file-system.js";
 import { getPermission } from "../../lib/permission.js";
 import dirname from "../../lib/dirname.js";
@@ -35,17 +36,56 @@ function apply(hook) {
             }
         }
         if (!subCmd) {
-            e.data.reply(sendMsg(e.bot, e.data, e.rawArgs));
+            e.data.reply(sendMsg(e.bot, e.data, e.rawArgs, e.data.message));
         }
     });
 }
 
-function sendMsg(bot, data, content) {
-    if (content.trim().length === 0) return "无字天书我看不懂哎";
+function sendMsg(bot, data, content, message) {
+    if (content.trim().length === 0) return "你好像什么也没说哦";
     const where = data.group_name;
     const who = data.sender.nickname;
-    bot.sendPrivateMsg(owner, `[${where}(${data.group_id})][${who}(${data.user_id})]:\n${content}`);
+    let msg = [`[${where}(${data.group_id})][${who}(${data.user_id})]:\n`];
+    msg.push(buildSendableMsg(removeCmd(message)));
+    bot.sendPrivateMsg(owner, _.flattenDeep(msg));
     return `已经反馈给我的主人咯~`;
+}
+
+
+function removeCmd(message) {
+    const botEnv = loadFileAsJson("data/bot-env.json");
+    const cfg = loadFileAsJson([__dirname, "config.json"]);
+    const cmdList = cfg.cmd;
+    const cmdFlag = (cfg.permission.indexOf("member") === -1) ? botEnv.adminCmdFlag : botEnv.cmdFlag;
+    let mesTemp = message[0];
+    if (mesTemp.type != "text") return message;
+    for (let f = 0; f < cmdFlag.length; ++f) {
+        for (let c = 0; c < cmdList.length; ++c) {
+            if (mesTemp.text.indexOf(cmdFlag[f] + cmdList[c]) == 0) {
+                mesTemp.text = mesTemp.text.slice((cmdFlag[f] + cmdList[c]).length).trimStart();
+                break;
+            }
+        }
+    }
+    message[0] = mesTemp;
+    return message;
+}
+
+function buildSendableMsg(message) {
+    let replyMsg = [];
+    message.forEach(m => {
+        if (m.type === 'text') {
+            replyMsg.push(m?.text);
+        } else if (m.type === 'face') {
+            replyMsg.push(segment.face(m?.id));
+        } else if (m.type === 'image') {
+            replyMsg.push(segment.image(m?.url));
+        } else if (m.type === 'at') {
+            replyMsg.push(segment.at(m?.qq));
+        } else {
+        }
+    });
+    return replyMsg;
 }
 
 export { apply };
